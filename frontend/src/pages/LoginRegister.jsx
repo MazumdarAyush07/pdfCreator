dc1603f7ce3f62f1a41b1d7fdd6d5c08a888dd7b
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2024-03-05 11:54:46 +0530

    add protection to unauthorized access

--- frontend/src/pages/LoginRegister.jsx
+++ frontend/src/pages/LoginRegister.jsx
@@ -72,147 +72,151 @@ const LoginRegister = () => {
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-br from-blue-300 to-blue-500">
-      <div className="bg-white rounded shadow-md w-full sm:w-96">
-        <div className="flex border-b">
-          <button
-            className={`w-1/2 py-2 ${
-              activeTab === "login" ? "bg-blue-500 text-white" : "text-blue-500"
-            }`}
-            onClick={() => handleTabChange("login")}
-          >
-            Login
-          </button>
-          <button
-            className={`w-1/2 py-2 ${
-              activeTab === "register"
-                ? "bg-blue-500 text-white"
-                : "text-blue-500"
-            }`}
-            onClick={() => handleTabChange("register")}
-          >
-            Register
-          </button>
-        </div>
-        <div className="p-6">
-          {error && <div className="text-red-600 mb-4">{error}</div>}
-          {successMessage && (
-            <div className="text-green-600 mb-4">{successMessage}</div>
-          )}
-          {activeTab === "login" && (
-            <form onSubmit={handleLoginSubmit}>
-              <div className="mb-4">
-                <label
-                  htmlFor="loginEmail"
-                  className="block text-gray-700 mb-2"
-                >
-                  Email:
-                </label>
-                <input
-                  id="loginEmail"
-                  type="email"
-                  value={loginEmail}
-                  onChange={(e) => setLoginEmail(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  required
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="loginPassword"
-                  className="block text-gray-700 mb-2"
-                >
-                  Password:
-                </label>
-                <input
-                  id="loginPassword"
-                  type="password"
-                  value={loginPassword}
-                  onChange={(e) => setLoginPassword(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  required
-                />
-              </div>
-              {successFlag ? (
-                <div>
-                  <Link to="/tools">
-                    <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">
-                      Tools
-                    </button>
-                  </Link>
-                  <Link to="/profile">
-                    <button className="w-full bg-blue-500 text-white py-2 rounded mt-2 hover:bg-blue-600 transition duration-300">
-                      Profile
-                    </button>
-                  </Link>
+      <div className="container px-4 mx-auto">
+        <div className="bg-white rounded shadow-md sm:w-96 mx-auto">
+          <div className="flex border-b">
+            <button
+              className={`w-1/2 py-2 ${
+                activeTab === "login"
+                  ? "bg-blue-500 text-white"
+                  : "text-blue-500"
+              }`}
+              onClick={() => handleTabChange("login")}
+            >
+              Login
+            </button>
+            <button
+              className={`w-1/2 py-2 ${
+                activeTab === "register"
+                  ? "bg-blue-500 text-white"
+                  : "text-blue-500"
+              }`}
+              onClick={() => handleTabChange("register")}
+            >
+              Register
+            </button>
+          </div>
+          <div className="p-6">
+            {error && <div className="text-red-600 mb-4">{error}</div>}
+            {successMessage && (
+              <div className="text-green-600 mb-4">{successMessage}</div>
+            )}
+            {activeTab === "login" && (
+              <form onSubmit={handleLoginSubmit}>
+                <div className="mb-4">
+                  <label
+                    htmlFor="loginEmail"
+                    className="block text-gray-700 mb-2"
+                  >
+                    Email:
+                  </label>
+                  <input
+                    id="loginEmail"
+                    type="email"
+                    value={loginEmail}
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                    className="w-full px-3 py-2 border rounded"
+                    required
+                  />
                 </div>
-              ) : (
+                <div className="mb-6">
+                  <label
+                    htmlFor="loginPassword"
+                    className="block text-gray-700 mb-2"
+                  >
+                    Password:
+                  </label>
+                  <input
+                    id="loginPassword"
+                    type="password"
+                    value={loginPassword}
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                    className="w-full px-3 py-2 border rounded"
+                    required
+                  />
+                </div>
+                {successFlag ? (
+                  <div>
+                    <Link to="/tools">
+                      <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">
+                        Tools
+                      </button>
+                    </Link>
+                    <Link to="/profile">
+                      <button className="w-full bg-blue-500 text-white py-2 rounded mt-2 hover:bg-blue-600 transition duration-300">
+                        Profile
+                      </button>
+                    </Link>
+                  </div>
+                ) : (
+                  <button
+                    type="submit"
+                    className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300"
+                  >
+                    Login
+                  </button>
+                )}
+              </form>
+            )}
+            {activeTab === "register" && (
+              <form onSubmit={handleRegisterSubmit}>
+                <div className="mb-4">
+                  <label
+                    htmlFor="registerUsername"
+                    className="block text-gray-700 mb-2"
+                  >
+                    Username:
+                  </label>
+                  <input
+                    id="registerUsername"
+                    type="text"
+                    value={registerUsername}
+                    onChange={(e) => setRegisterUsername(e.target.value)}
+                    className="w-full px-3 py-2 border rounded"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="registerEmail"
+                    className="block text-gray-700 mb-2"
+                  >
+                    Email:
+                  </label>
+                  <input
+                    id="registerEmail"
+                    type="email"
+                    value={registerEmail}
+                    onChange={(e) => setRegisterEmail(e.target.value)}
+                    className="w-full px-3 py-2 border rounded"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <label
+                    htmlFor="registerPassword"
+                    className="block text-gray-700 mb-2"
+                  >
+                    Password:
+                  </label>
+                  <input
+                    id="registerPassword"
+                    type="password"
+                    value={registerPassword}
+                    onChange={(e) => setRegisterPassword(e.target.value)}
+                    className="w-full px-3 py-2 border rounded"
+                    required
+                  />
+                </div>
                 <button
                   type="submit"
                   className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300"
                 >
-                  Login
+                  Register
                 </button>
-              )}
-            </form>
-          )}
-          {activeTab === "register" && (
-            <form onSubmit={handleRegisterSubmit}>
-              <div className="mb-4">
-                <label
-                  htmlFor="registerUsername"
-                  className="block text-gray-700 mb-2"
-                >
-                  Username:
-                </label>
-                <input
-                  id="registerUsername"
-                  type="text"
-                  value={registerUsername}
-                  onChange={(e) => setRegisterUsername(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="registerEmail"
-                  className="block text-gray-700 mb-2"
-                >
-                  Email:
-                </label>
-                <input
-                  id="registerEmail"
-                  type="email"
-                  value={registerEmail}
-                  onChange={(e) => setRegisterEmail(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  required
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="registerPassword"
-                  className="block text-gray-700 mb-2"
-                >
-                  Password:
-                </label>
-                <input
-                  id="registerPassword"
-                  type="password"
-                  value={registerPassword}
-                  onChange={(e) => setRegisterPassword(e.target.value)}
-                  className="w-full px-3 py-2 border rounded"
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300"
-              >
-                Register
-              </button>
-            </form>
-          )}
+              </form>
+            )}
+          </div>
         </div>
       </div>
     </div>
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,154 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Profile = () => {
+  const [userData, setUserData] = useState(null);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      window.location.href = "/";
+    }
+  }, []);
+
+  const fetchUserData = async () => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) {
+      console.error("Not authenticated");
+      return;
+    }
+
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/v1/users/get-currentuser",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setUserData(response.data.data);
+    } catch (error) {
+      alert(`Error! ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const handleDelete = async (fileId, index) => {
+    try {
+      setLoading(true);
+      const token = localStorage.getItem("accessToken");
+      const response = await axios.delete(
+        `http://localhost:8000/api/v1/file/delete/${fileId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data.message);
+      setUserData((prevUserData) => ({
+        ...prevUserData,
+        files: prevUserData.files.filter((_, i) => i !== index),
+      }));
+    } catch (error) {
+      alert(`Error! ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("user");
+  };
+
+  return (
+    <div className="bg-gradient-to-br from-blue-400 to-blue-700 min-h-screen flex justify-center items-center">
+      <div className="bg-gradient-to-br from-blue-600 to-blue-900 p-6 rounded-lg text-white max-w-md mx-auto">
+        <div className="text-2xl font-bold mb-6">Profile</div>
+        <div className="mb-4">
+          <p className="font-bold">Username:</p>
+          <p>{user.username}</p>
+        </div>
+        <div className="mb-4">
+          <p className="font-bold">Email:</p>
+          <p>{user.email}</p>
+        </div>
+        <div className="text-center mb-4">
+          <button
+            className="bg-white text-blue-600 px-4 py-2 rounded-md shadow-md hover:bg-blue-600 hover:text-white transition duration-300"
+            onClick={fetchUserData}
+            disabled={loading}
+          >
+            View Details
+          </button>
+        </div>
+        {userData && userData.files && (
+          <div className="mt-6">
+            <table className="table-auto">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2">Original</th>
+                  <th className="px-4 py-2">Modified</th>
+                  <th className="px-4 py-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userData.files.map((file, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">
+                      <a
+                        href={file.orginal}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {file.name}
+                      </a>
+                    </td>
+                    <td className="border px-4 py-2">
+                      <a
+                        href={file.modified}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Modified Link
+                      </a>
+                    </td>
+                    <td className="border px-4 py-2">
+                      <button
+                        className={`${
+                          loading ? "opacity-50 cursor-not-allowed" : ""
+                        } bg-red-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-600 transition duration-300`}
+                        onClick={() => handleDelete(file._id, index)}
+                        disabled={loading}
+                      >
+                        {loading ? "Deleting..." : "Delete"}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        <div className="text-center mt-4">
+          <button
+            className="bg-gray-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-gray-600 transition duration-300"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
