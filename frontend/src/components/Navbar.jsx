3e7dd2fd77fbfbaaffe53b3d0eddd073b4c3ad16
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2024-03-05 12:13:33 +0530

    add responsiveness to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import pdflogo from "../assets/pdf-logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-blue-500 to-blue-700 py-4 px-10">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
+    <nav className="bg-gradient-to-r from-blue-500 to-blue-700 py-4 px-4 lg:px-10">
+      <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
+        <div className="flex items-center mb-4 lg:mb-0">
           <img src={pdflogo} alt="Logo" className="w-10 h-10 mr-2" />
-          <h1 className="text-white text-3xl font-bold">pdfCreator</h1>
+          <h1 className="text-white text-2xl lg:text-3xl font-bold">
+            pdfCreator
+          </h1>
         </div>
         <div className="flex items-center">
           <Link
             to="/tools"
-            className="text-white font-semibold mr-6 hover:text-blue-200"
+            className="text-white font-semibold mr-4 lg:mr-6 hover:text-blue-200"
           >
             PDF Modifier
           </Link>
